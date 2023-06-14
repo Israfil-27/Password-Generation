5e9557529329d496fc7bb4d4734c5afc9739db6e
israfil <israfik666@gmail.com>
2023-06-14 07:55:46 -0700

    dasd

--- javascript/app.js
+++ javascript/app.js
@@ -1,33 +1,47 @@
-const passwordLength=document.querySelector(".passwordlength input")
-generateBtn=document.querySelector(".button-password")
-options=document.querySelectorAll(".option input")
+const passwordLength = document.querySelector(".passwordlength input");
+const generateBtn = document.querySelector(".button-password");
+const options = document.querySelectorAll(".option input");
+const lengthslider = document.querySelector(".passindigator");
 
-
-
-const updatepasslength=()=>{
-let passwordLength1=document.querySelector(".passlength span")
-passwordLength1.innerText=passwordLength.value
-}
-let caracterData={
-    Lovercase:"qwertyuiopasdfghjklzxcvbnm",
-    Uppercase:"QWERTYUIOPASDFGHJKLZXCVBNM",
-    number:"1234567890",
-    Symbol:"()_+{}?><~`[];.,-="
-}
-const PasswordGenerate=()=>{
-    let staticPassword=""
-    randomPassword=""
-    options.forEach(option => {
-        if(option.checked){
-            staticPassword+=caracterData[option.id]
-        }
-    });
-    for (let i = 0; i < passwordLength.value; i++) {
-        randomPassword+=staticPassword[Math.floor(Math.random()*staticPassword.length)]
+const updatepasslength = () => {
+  let passwordLength1 = document.querySelector(".passlength span");
+  passwordLength1.innerText = passwordLength.value;
+};
+let caracterData = {
+  Lovercase: "qwertyuiopasdfghjklzxcvbnm",
+  Uppercase: "QWERTYUIOPASDFGHJKLZXCVBNM",
+  number: "1234567890",
+  Symbol: "()_+{}?><~`[];.,-=",
+};
+const PasswordGenerate = () => {
+  const passwordText = document.querySelector(".passtext input");
+  let staticPassword = "";
+  randomPassword = "";
+  options.forEach((option) => {
+    if (option.checked) {
+      staticPassword += caracterData[option.id];
     }
-    console.log(randomPassword)
-   
-}
-PasswordGenerate()
-generateBtn.addEventListener("click",PasswordGenerate)
-passwordLength.addEventListener("input",updatepasslength)
+  });
+  for (let i = 0; i < passwordLength.value; i++) {
+    randomPassword +=
+      staticPassword[Math.floor(Math.random() * staticPassword.length)];
+  }
+  passwordText.value = randomPassword;
+  passindigatorlength();
+};
+const passindigatorlength = () => {
+  if (passwordLength.value < 8) {
+    // lengthslider.id = "red";
+    lengthslider.style.background = "red";
+    lengthslider.style.width = "33%";
+  } else if (passwordLength.value < 16) {
+    // lengthslider.id = "yellow";
+    lengthslider.style.background = "yellow";
+  } else {
+    // lengthslider.id = "green";
+    lengthslider.style.background = "green";
+  }
+};
+
+generateBtn.addEventListener("click", PasswordGenerate);
+passwordLength.addEventListener("input", updatepasslength);
