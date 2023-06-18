579f82b58c1881d685290d6cf1cd350fe7b703c0
israfil <israfik666@gmail.com>
2023-06-18 10:13:51 -0700

    change copy icon

--- javascript/app.js
+++ javascript/app.js
@@ -2,6 +2,7 @@ const passwordLength = document.querySelector(".passwordlength input");
 const generateBtn = document.querySelector(".button-password");
 const options = document.querySelectorAll(".option input");
 const lengthslider = document.querySelector(".passindigator");
+const copy = document.querySelector(".passtext span");
 
 const updatepasslength = () => {
   let passwordLength1 = document.querySelector(".passlength span");
@@ -13,10 +14,11 @@ let caracterData = {
   number: "1234567890",
   Symbol: "()_+{}?><~`[];.,-=",
 };
+let staticPassword = "";
+let randomPassword = "";
 const PasswordGenerate = () => {
-  const passwordText = document.querySelector(".passtext input");
-  let staticPassword = "";
   randomPassword = "";
+  const passwordText = document.querySelector(".passtext input");
   options.forEach((option) => {
     if (option.checked) {
       staticPassword += caracterData[option.id];
@@ -29,17 +31,24 @@ const PasswordGenerate = () => {
   passwordText.value = randomPassword;
   passindigatorlength();
 };
+
 const passindigatorlength = () => {
   if (passwordLength.value < 8) {
     lengthslider.id = "red";
   } else if (passwordLength.value < 16) {
     lengthslider.id = "yellow";
-    
   } else {
     lengthslider.id = "green";
-  
   }
 };
+const Passwordclick = () => {
+  navigator.clipboard.writeText(randomPassword);
+  copy.textContent = "done";
+  setTimeout(() => {
+    copy.textContent = "content_copy";
+  }, 3000);
+};
 
+copy.addEventListener("click", Passwordclick);
 generateBtn.addEventListener("click", PasswordGenerate);
 passwordLength.addEventListener("input", updatepasslength);
